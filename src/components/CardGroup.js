fcce5533bbb5748b638e23dba770978f5b59fcca
Daniel Chell <dgchell@gmail.com>
2020-09-21 01:56:03 -0500

    MVP complete

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -7,17 +7,32 @@ class CardGroup extends Component {
       <div class="cardDeckContainer">
         <CardDeck>
           <Card>
-            <Card.Img
+            {/* <Card.Img
               variant="top"
               src="https://camo.githubusercontent.com/b716eb1b6d971a7c0cdff77baa13d771ff340d56/68747470733a2f2f692e696d6775722e636f6d2f4a6d30423938662e706e67"
-            />
+            /> */}
             <Card.Body>
               <Card.Title>TopoGraphQL</Card.Title>
               <Card.Text>
                 TopoGraphQL is an open-source GraphQL IDE, schema visualizer,
                 and endpoint analytics tool. Inpsired by GraphiQL,TopoGraphQL
                 provides developers a streamlined, in-browser solution for
-                navigating and testing complex GraphQL APIs and queries.
+                navigating and testing complex GraphQL APIs and queries. Built
+                Using:
+                <ul>
+                  <li>React</li>
+                  <li>D3</li>
+                  <li>Ant Design Library</li>
+                  <li>Amazon Web Services</li>
+                  <li>Docker</li>
+                  <li>GraphQL</li>
+                  <li>SQL</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>Bcrypt</li>
+                  <li>JWTs</li>
+                  <li>Webpack</li>
+                </ul>
               </Card.Text>
             </Card.Body>
             <Card.Footer>
@@ -30,43 +45,64 @@ class CardGroup extends Component {
             </Card.Footer>
           </Card>
           <Card>
-            <Card.Img
+            {/* <Card.Img
               variant="top"
               src="https://scontent.ffsd3-1.fna.fbcdn.net/v/t1.0-9/115772182_10158278516435792_8509649749366949457_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_ohc=VQYeTGRgJbMAX-1EveG&_nc_ht=scontent.ffsd3-1.fna&oh=9d13467f22fbf13fbfc95845acc104a7&oe=5F842B5B"
-            />
+            /> */}
             <Card.Body>
               <Card.Title>Dev Marketplace</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
+                Dev Marketplace is an online portal for software engineers and
+                employers. It features a job postings board, along with video
+                chat functionality. Built Using:
+                <ul>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Socket.io</li>
+                  <li>Javascript</li>
+                  <li>Postgres</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>Webpack</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                </ul>
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <Card.Link href="#">Visit Project</Card.Link>
+              {/* <Card.Link href="#">Visit Project</Card.Link> */}
               <Card.Link href="https://github.com/Porcupine-Software-Solutions/devMarketplace">
                 See Code
               </Card.Link>
             </Card.Footer>
           </Card>
           <Card>
-            <Card.Img
+            {/* <Card.Img
               variant="top"
               src="https://scontent.ffsd3-1.fna.fbcdn.net/v/t1.0-9/115772182_10158278516435792_8509649749366949457_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_ohc=VQYeTGRgJbMAX-1EveG&_nc_ht=scontent.ffsd3-1.fna&oh=9d13467f22fbf13fbfc95845acc104a7&oe=5F842B5B"
-            />
+            /> */}
             <Card.Body>
               <Card.Title>YelpCamp</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
+                YelpCamp is a web application for providing campsite information
+                and reviews. Built Using:
+                <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Javascript</li>
+                  <li>Bootstrap</li>
+                  <li>MongoDB</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>Heroku</li>
+                </ul>
               </Card.Text>
             </Card.Body>
             <Card.Footer>
               <Card.Link href="https://nameless-shore-35445.herokuapp.com/">
                 Visit Project
               </Card.Link>
-              <Card.Link href="#">See Code</Card.Link>
+              {/* <Card.Link href="#">See Code</Card.Link> */}
             </Card.Footer>
           </Card>
         </CardDeck>
